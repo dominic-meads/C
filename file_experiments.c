de2e2283656f53720fc272d372b7758f967bb8dd
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2020-06-14 01:18:40 -0700

    added fscanf stuff in addition to fprintf

--- file_experiments.c
+++ file_experiments.c
@@ -1,8 +1,8 @@
 // Just a quick messaround sketch printing and reading to files to help me get better at C
 
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include <math.h>    // for rand()
+#include <stdlib.h>  // for files
 
 
 
@@ -10,10 +10,11 @@ int main()
 {
     float my_array[8] = {};
     float max = 2147483647;  // RAND_MAX
+    float read_array[8] = {};  // to put the read values into.
     
     FILE * fp; 
     
-    fp = fopen("MyRandomNumbers.txt","w");  // open/create file
+    fp = fopen("MyRandomNumbers.txt","w+");  // open/create file (w+ means write or read)
     
     for(int i = 0; i < 8; i++)
     {
@@ -21,7 +22,16 @@ int main()
         fprintf(fp,"%f\n",my_array[i]);  // print a random number to the file
     }
     
+    rewind(fp);  // reset position to beginning of file 
+    
+    for(int i = 0; i < 8; i++)
+    {
+        fscanf(fp,"%f", &read_array[i]);  // read each line of file and store in array
+        printf("line %d of the file holds the number %f\n", i, read_array[i]);  // print the values 
+    }
+    
+    
     fclose(fp);  // close file
-
+    
     return 0;
 }
